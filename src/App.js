b7f92e46de190c07460e5d76e622f990e248d8cf
Sujana Bhandari <sujana6589@gmail.com>
2022-09-16 08:31:53 +0200

    Add header and footer design

--- src/App.js
+++ src/App.js
@@ -2,14 +2,15 @@ import './App.css';
 import Register from './Componets/Register';
 import Navbar from './Componets/Navbar';
 import Login from './Componets/login';
-
+import Footer from './Componets/Footer';
 function App() {
   return (
     <div className="App">
       <Navbar />
-      <h1 className='text-primary' style={{ marginTop: '100px'}}>Hello Home Made App</h1>
+      {/* <h1 className='text-primary' style={{ marginTop: '100px'}}>Hello Home Made App</h1> */}
       <Register />
-      <Login />
+      {/* <Login /> */}
+      <Footer />
     </div>
   );
 }
