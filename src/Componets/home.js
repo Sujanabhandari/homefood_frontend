1ee95e077f451acb4294adfbdcdf5c0c8a32fe9a
Sujana Bhandari <sujana6589@gmail.com>
2022-09-21 15:17:53 +0200

    Testing

--- src/Componets/home.js
+++ src/Componets/home.js
@@ -22,7 +22,7 @@ const Home = ({posts}) =>  {
               </div>
             </div>
           </div>
-
+          
           <div className="col-lg-6 col-md-8 mx-auto specialOffer">
             <div className="row">
               <div className="col-lg-6">
