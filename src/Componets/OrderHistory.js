3b4c436335d35f51e9da2a2b6547c1731de4cfcd
Sujana Bhandari <sujana6589@gmail.com>
2022-09-27 12:37:59 +0200

    Display order recieved and Order history

--- src/Componets/OrderHistory.js
+++ src/Componets/OrderHistory.js
@@ -3,37 +3,44 @@ import React, { Component } from 'react';
 import { Link, useLocation } from "react-router-dom";
 import ReactStars from "react-rating-stars-component";
 import { useEffect } from 'react';
+import { useHomeContext } from './MainContext';
+
 const ratingChanged = (newRating) => {
   console.log("Rating", newRating);
 };
 
 const OrderHistory = () => {
-  
-  console.log("Form Accoutn",useLocation())
-  const {state:{orders}} = useLocation()
+  const { user } = useHomeContext();
+  const { state } = useLocation()
 
-  // const data = state;
-  console.log(orders);
+  console.log("From Order History", state.customerId);
+  console.log("From User History", user);
 
-    return (
+
+  const offerHistory = state?.filter((history) => history.customerId._id == user._id);
+  console.log("Form Comparioson", offerHistory);
+
+
+  return (
     <>
       <section className="container">
         <h1 className="mb-3">My order history</h1>
-        <div className="row">
-          <div className="col">
-            <div className="d-flex flex-column flex-md-row flex-row justify-content-between align-items-center orderedFood p-3">
-              <div className="p-2">
-                <div className="wrapperImg d-block d-md-inline-block mx-auto">
-                  <img src="https://web-assets.bcg.com/3c/3d/794ddde7481695d246407d66e179/food-for-thought-the-untapped-climate-opportunity-in-alternative-proteins-rectangle.jpg" />
+        {offerHistory?.map((offerHistory, index) => (
+          <><div className="row">
+            <div className="col">
+              <div className="d-flex flex-column flex-md-row flex-row justify-content-between align-items-center orderedFood p-3">
+                <div className="p-2">
+                  <div className="wrapperImg d-block d-md-inline-block mx-auto">
+                    <img src="https://web-assets.bcg.com/3c/3d/794ddde7481695d246407d66e179/food-for-thought-the-untapped-climate-opportunity-in-alternative-proteins-rectangle.jpg" />
+                  </div>
+                  <span className="foodTitle"> {offerHistory.offerId.title}</span>
                 </div>
-                <span className="foodTitle"> Title of food</span>
-              </div>
-              <div className="p-2">Quantity: 2</div>
-              <div className="p-2">Total price: 15€</div>
-              <div className="p-2">Order date: 14.9.2022</div>
-              <div className="p-2">Home Made by: Sujana</div>
-              <div className="p-2 ratingBar">
-                <span>Rate creator: </span>
+                <div className="p-2">Quantity:{offerHistory.order_quantity}</div>
+                {/* <div className="p-2">Total price: 15€</div> */}
+                <div className="p-2">Order date: 14.9.2022</div>
+                <div className="p-2">Home Made by: {offerHistory.creatorId.userName}</div>
+                <div className="p-2 ratingBar">
+                  <span>Rate creator: </span>
                   <ReactStars
                     count={5}
                     onChange={ratingChanged}
@@ -42,47 +49,18 @@ const OrderHistory = () => {
                     emptyIcon={<i className="far fa-star"></i>}
                     halfIcon={<i className="fa fa-star-half-alt"></i>}
                     fullIcon={<i className="fa fa-star"></i>}
-                    activeColor="#ffd700"
-                  />
-              </div>
-            </div>
-          </div>
-        </div>
-
-        <div className="row mt-2">
-          <div className="col">
-            <div className="d-flex flex-column flex-md-row flex-row justify-content-between align-items-center orderedFood p-3">
-              <div className="p-2">
-                <div className="wrapperImg d-block d-md-inline-block mx-auto">
-                  <img src="https://web-assets.bcg.com/3c/3d/794ddde7481695d246407d66e179/food-for-thought-the-untapped-climate-opportunity-in-alternative-proteins-rectangle.jpg" />
+                    activeColor="#ffd700" />
                 </div>
-                <span className="foodTitle"> Title of food</span>
-              </div>
-              <div className="p-2">Quantity: 2</div>
-              <div className="p-2">Total price: 15€</div>
-              <div className="p-2">Order date: 14.9.2022</div>
-              <div className="p-2">Home Made by: Sujana</div>
-              <div className="p-2 ratingBar">
-                <span>Rate creator: </span>
-                <ReactStars
-                    count={5}
-                    onChange={ratingChanged}
-                    size={24}
-                    isHalf={true}
-                    emptyIcon={<i className="far fa-star"></i>}
-                    halfIcon={<i className="fa fa-star-half-alt"></i>}
-                    fullIcon={<i className="fa fa-star"></i>}
-                    activeColor="#ffd700"
-                  />
               </div>
             </div>
           </div>
-        </div>
 
+          </>
+        ))}
       </section>
     </>
-    )
-  
+  )
+
 }
 
 export default OrderHistory;
--- src/Componets/OrdersReceived.js
+++ src/Componets/OrdersReceived.js
@@ -1,31 +1,27 @@
 /* eslint-disable jsx-a11y/alt-text */
 import React, { Component } from 'react';
 import { Link, useLocation } from "react-router-dom";
+import { useHomeContext } from './MainContext';
+
 
 const OrdersReceived = () => {
 
+  const { user } = useHomeContext();
+  const { state } = useLocation()
+
+  console.log("From Order Recieved", state);
+  console.log("From User Recieved", user);
+
+
+  const offerHistory = state?.filter((history) => history.creatorId._id == user._id);
+  console.log("Form Comparioson", offerHistory);
+
+  
     return (
     <>
       <section className="container dashboard">
         <h1 className="mb-3">Orders Received</h1>
-        <div className="row">
-          <div className="col">
-            <div className="d-flex flex-column flex-md-row flex-row justify-content-between align-items-center ordersReceived p-3">
-            <div className="p-2">
-              <div className="wrapperImg d-block d-md-inline-block mx-auto">
-                <img src="https://web-assets.bcg.com/3c/3d/794ddde7481695d246407d66e179/food-for-thought-the-untapped-climate-opportunity-in-alternative-proteins-rectangle.jpg" />
-              </div>
-              <span className="foodTitle"> Title of food</span>
-            </div>
-            <div className="p-2">Quantity: 2</div>
-            <div className="p-2">Total price: 15€</div>
-            <div className="p-2">Order date: 14.9.2022</div>
-            <div className="p-2">Pay method: cash</div>
-            <div className="p-2">Ordered by: Sujana</div>
-            </div>
-          </div>
-        </div>
-
+        {offerHistory?.map((offerHistory, index) => (
         <div className="row mt-2">
           <div className="col">
             <div className="d-flex flex-column flex-md-row flex-row justify-content-between align-items-center ordersReceived p-3">
@@ -33,16 +29,17 @@ const OrdersReceived = () => {
                 <div className="wrapperImg d-block d-md-inline-block mx-auto">
                   <img src="https://web-assets.bcg.com/3c/3d/794ddde7481695d246407d66e179/food-for-thought-the-untapped-climate-opportunity-in-alternative-proteins-rectangle.jpg" />
                 </div>
-                <span className="foodTitle"> Title of food</span>
+                <span className="foodTitle"> {offerHistory.offerId.title}</span>
               </div>
-              <div className="p-2">Quantity: 2</div>
-              <div className="p-2">Total price: 15€</div>
-              <div className="p-2">Order date: 14.9.2022</div>
-              <div className="p-2">Pay method: cash</div>
-              <div className="p-2">Ordered by: Sujana</div>
+              <div className="p-2">Quantity:{offerHistory.offerId.order_quantity}</div>
+              {/* <div className="p-2">Total price: 15€</div> */}
+              <div className="p-2">Order date: {offerHistory.offerId.date}</div>
+              {/* <div className="p-2">Pay method: cash</div> */}
+              <div className="p-2">Ordered by: {offerHistory.customerId.userName}</div>
             </div>
           </div>
         </div>
+        ))}
 
       </section>
     </>
